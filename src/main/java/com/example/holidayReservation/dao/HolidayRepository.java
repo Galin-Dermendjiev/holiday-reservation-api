4ce2d681113cc11e3fe92f1f661d5935bf65a5b4
Galin <sao4449@abv.bg>
2024-01-13 00:52:54 +0200

    Delete files

--- src/main/java/com/example/holidayReservation/dao/HolidayRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.holidayReservation.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.holidayReservation.model.Holiday;
-
-public interface HolidayRepository extends JpaRepository<Holiday, Long> {
-}
--- src/main/java/com/example/holidayReservation/dao/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.holidayReservation.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.holidayReservation.model.Reservation;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
-}
